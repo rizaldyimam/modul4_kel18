1d7a373e489b427284b4df38f7cb63499eaca348
Rizaldy Imam Khadafi <khadafiimam574@gmail.com>
2020-04-18 23:21:30 +0700

    Tugas Modul 4

---
+++ Tugas modul 4/Pendefinisi.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Tugas_4
+{
+    class Pendefinisi
+    {
+        public void prima(int n)
+        {
+            bool prima;
+            int jumlah, i, nilai = 0;
+
+            jumlah = 1;
+            i = 2;
+
+            while (jumlah <= n)
+            {
+                prima = true;
+                for (int x = 2; x < i; x++)
+                {
+                    if (i % x == 0)
+                    {
+                        prima = false;
+                    }
+                }
+
+                if (prima == true)
+                {
+                    nilai = nilai + i;
+                    Console.Write("{0} ", i);
+                    jumlah++;
+                }
+
+                i++;
+            }
+            Console.WriteLine("\nJumlah Bilangan Prima yang telah di tampilkan adalah : {0}", nilai);
+        }
+
+    }
+}
---
+++ Tugas modul 4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tugas_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        start:
+            string a;
+            int x;
+            Console.WriteLine("\nProgram Bilangan Prima Kelompok 18");
+            Console.Write("Ada berapa bilangan prima yang anda ingin tampilkan ??\nBanyak : ");
+            a = Console.ReadLine();
+            int.TryParse(a, out x);
+            if (x <= 1)
+            {
+                Console.WriteLine("Maaf, Tetapi bilangan prima terkecil adalah 2");
+                Console.ReadKey();
+                goto start;
+
+            }
+            else
+            {
+                Pendefinisi objek = new Pendefinisi();
+                Console.WriteLine("Bilangan prima : ");
+                objek.prima(x);
+                goto start;
+            }
+
+        }
+    }
+}
